74faabedfc108a05da9890aeb717c42338214d30
Leony Leal <leony.leal@outlook.com>
2022-09-22 14:35:21 -0300

    Removing Initial styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "styled-components"
+import GlobalStyled from ''
 
 function MyApp({ Component, pageProps }) {
+  <ThemeProvider>
+    <GlobalStyles>
   return <Component {...pageProps} />
+    </GlobalStyles>
+  </ThemeProvider>
 }
 
 export default MyApp
