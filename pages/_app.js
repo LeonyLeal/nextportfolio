d38c0968c0405c5f012d721d954a0f02a21850eb
Leony Leal <leony.leal@outlook.com>
2022-09-22 17:28:17 -0300

    Meta and Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { ThemeProvider } from "styled-components"
-import {GlobalStyles} from '../styles/GlobalStyles'
-import dark from '../styles/theme/dark'
-import light from '../styles/theme/light'
-import Navbar from "../components/Navbar"
-import { useState } from "react"
-import Head from "next/head"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styles/GlobalStyles";
+import dark from "../styles/theme/dark";
+import light from "../styles/theme/light";
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState(dark);
@@ -12,19 +12,21 @@ function MyApp({ Component, pageProps }) {
   const onThemeClick = () => {
     setTheme(theme.title === "dark" ? light : dark);
   };
-  return(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles/>
-    <>
-    <Head>
-      <title>Leony</title>
-    </Head>
-     <Navbar onChangeTheme={onThemeClick} />
-     <Component {...pageProps} />
-     </>
-  </ThemeProvider>
-  
-  )
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <>
+        <Head>
+          <title>Leony</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <meta name='description' content='Portifólio Leony Leal, Desenvolvedor Fullstack, Trabalho com React, Next, Vue, Django, Axios' />
+        </Head>
+        <Navbar onChangeTheme={onThemeClick} />
+        <Component {...pageProps} />
+      </>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
