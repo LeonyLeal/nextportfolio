a38552b4e147017485bf0c629e1e2365fb7bf57f
Leony Leal <leony.leal@outlook.com>
2026-01-21 04:19:34 -0300

    chore: test build

--- sections/Projectx/index.js
+++ sections/Projectx/index.js
@@ -7,12 +7,12 @@ function Projectxs(props) {
     <ProjectWrapper id="Section-D">
       <Title id="Projects">Github</Title>
       <Grid>
-        {props.repos
-          .filter(
+        {props.repos?.filter(
             (item) =>
-              !item.topics.some(topic => props.ForbiddenTopics.includes(topic)),
-          )
-          .map((repo) => (
+              !item.topics.some((topic) =>
+                props.ForbiddenTopics.includes(topic),
+              ),
+          ).map((repo) => (
             <ProjectContainer key={repo.id} id={repo.name}>
               <Apps title={repo.name} preview={props.preview}>
                 <Link id={repo.name} target="_blank" href={repo.html_url}>
